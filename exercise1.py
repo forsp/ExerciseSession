7219c2bd79473da353015441be80acbd033b96f1
Ekaterina Koroteeva <forsp@mail.ru>
2012-02-22 14:44:32 +0100

    changes

--- exercise1.py
+++ exercise1.py
@@ -7,18 +7,19 @@ fold="cleaneddata"
 def listfiles(dir):
     return os.listdir(dir)
 
-def findmiss(file):
-    for line in fileinput.input(file):
-            array=line.split(":")
-            if len(array)==2 and array[1].strip()=="N":
-            return True
-            else:
-            return False
-
+def findmiss(file1):
+      #for line in file1.readlines():
+        line=file1.readlines()
+        if "Sex: N\n" in line:
+                 return True
+        else:
+                return False 
+  
 def main():
-    #print listfiles(fold)
-    for file in listfiles(fold):
-        print str(file) + ':' +str(findmiss(file))
+   # print listfiles(fold)
+ #   for file1 in listfiles(fold):
+       file1=open('cleaneddata/jamesm_data_360.txt')
+       print str(file1) + '-' +str(findmiss(file1))
 
 main()
 
